3282b139bc641977e6a7f5a01e9d942b87913fcc
jose <jose.csousa22@gmail.com>
2026-02-07 10:55:02 -0300

    Simplifica estrutura de Departments e ajusta relacionamentos

--- EmployeeManagement.Infrastructure/Migrations/20260207135346_Update_Department_Manager.Designer.cs
+++ EmployeeManagement.Infrastructure/Migrations/20260207135346_Update_Department_Manager.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EmployeeManagement.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EmployeeManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260207135346_Update_Department_Manager")]
+    partial class Update_Department_Manager
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmployeeManagement.Infrastructure/Migrations/20260207135346_Update_Department_Manager.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Department_Manager : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Departments_DepartmentId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Departments_ParentDepartmentId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Employees_ManagerId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Employees_ManagerId1",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departments_DepartmentId",
+                table: "Departments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departments_ManagerId1",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "ManagerId1",
+                table: "Departments");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ManagerId",
+                table: "Departments",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Departments_ParentDepartmentId",
+                table: "Departments",
+                column: "ParentDepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Employees_ManagerId",
+                table: "Departments",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Departments_ParentDepartmentId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Employees_ManagerId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ManagerId",
+                table: "Departments",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentId",
+                table: "Departments",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ManagerId1",
+                table: "Departments",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_DepartmentId",
+                table: "Departments",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_ManagerId1",
+                table: "Departments",
+                column: "ManagerId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Departments_DepartmentId",
+                table: "Departments",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Departments_ParentDepartmentId",
+                table: "Departments",
+                column: "ParentDepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Employees_ManagerId",
+                table: "Departments",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Employees_ManagerId1",
+                table: "Departments",
+                column: "ManagerId1",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
